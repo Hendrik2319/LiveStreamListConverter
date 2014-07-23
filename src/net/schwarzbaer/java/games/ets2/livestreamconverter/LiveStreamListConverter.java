bdf950da5e0ddf93a0dff0ae540ea6510ad76dce
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2014-07-23 23:17:58 +0000

    JAVA: [Eclipse] - Projekt "ETS2_LiveStreamListConverter"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/ETS2_LiveStreamListConverter@2318 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/games/ets2/livestreamconverter/LiveStreamListConverter.java
+++ src/net/schwarzbaer/java/games/ets2/livestreamconverter/LiveStreamListConverter.java
@@ -10,7 +10,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.net.MalformedURLException;
